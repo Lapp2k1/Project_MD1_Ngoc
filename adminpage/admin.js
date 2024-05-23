c50dacf20da9ebffc91ad784874fe31b75780024
Lapp2k1 <vulengocvip2015@gmail.com>
2024-05-23 17:35:28 +0700

    ver 1.0

--- adminpage/admin.js
+++ adminpage/admin.js
@@ -2,8 +2,8 @@ document.querySelector(".admin-img").addEventListener("click", function () {
   document.getElementById("adminMenu").style.display = "block";
 });
 document.querySelector(".log-out").addEventListener("click", function () {
-  localStorage.removeItem("userLogin");
   let userData = JSON.parse(localStorage.getItem("userData")) || [];
   userData[0].status = "inactive";
   localStorage.setItem("userData", JSON.stringify(userData));
+  localStorage.removeItem("userLogin");
 });
--- adminpage/manage-account/manage-account.js
+++ adminpage/manage-account/manage-account.js
@@ -11,13 +11,13 @@ document.querySelector(".log-out").addEventListener("click", function () {
 // -------------------table---------------------
 
 function render() {
-    const tableBody = document.getElementById('categoryBody');
-    const userData = JSON.parse(localStorage.getItem('userData'));
-  
-    let stringHTML = ''; // Initialize empty string for HTML
-  
-    userData.forEach((user, i) => {
-      stringHTML += `
+  const tableBody = document.getElementById("categoryBody");
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  let stringHTML = ""; // Initialize empty string for HTML
+
+  userData.forEach((user, i) => {
+    stringHTML += `
         <tr>
           <td>${i + 1}</td>
           <td>${user.userName}</td>
@@ -25,19 +25,22 @@ function render() {
           <td>${user.permission}</td>
           <td>${user.status}</td>
           <td>
-          ${user.permission === 'admin' ? '' : ` `}
-          ${user.permission !== 'admin' ? `
+          ${user.permission === "admin" ? "" : ` `}
+          ${
+            user.permission !== "admin"
+              ? `
             <button class="action-button update-button" data-user-id="${user.id}">Update</button>
             <button class="action-button block-button" data-user-id="${user.id}">Block</button>
             <button class="action-button delete-button" data-user-id="${user.id}">Delete</button>
-          ` : ''}
+          `
+              : ""
+          }
           </td>
         </tr>
       `;
-    });
-  
-    tableBody.innerHTML = stringHTML; // Set the table body HTML with the generated string
-  
-  }
-  render();
+  });
+
+  tableBody.innerHTML = stringHTML; // Set the table body HTML with the generated string
+}
+render();
 // ---------------------function sortuser--------------------
